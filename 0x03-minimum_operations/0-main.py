c13c903b62e4fb721526d33c50476036794c09ab
Miguelro123 <969@holbertonschool.com>
2020-05-20 04:28:08 +0000

    Minimum Operations

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -10,4 +10,3 @@ print("Min number of operations to reach {} characters: {}".format(n, minOperati
 
 n = 12
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,13 +3,13 @@
 
 def minOperations(n):
     p = 2
-    num = 0
+    integer = 0
     if type(n) != int or n <= 1:
         return 0
     while n != 1:
         if n % p == 0:
             n = n / p
-            num = num + p
+            integer = num + p
         else:
             p = p + 1
-    return int(num)
+    return integer
