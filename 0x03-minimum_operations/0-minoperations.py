e1c05e49ab538b8401d8c1f6b5f38403f4fb3a38
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-05-19 23:31:14 -0500

    Update 0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """minOperations"""
 
+
 def minOperations(n):
+    """number of operations to copy paste
+    Args:
+        n: given number
+    Returns:
+        num: number of operations.
+    """
     p = 2
-    integer = 0
+    num = 0
     if type(n) != int or n <= 1:
         return 0
     while n != 1:
         if n % p == 0:
             n = n / p
-            integer = num + p
+            num = num + p
         else:
             p = p + 1
-    return integer
+    return int(num)
