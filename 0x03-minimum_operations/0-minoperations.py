5e15a569e46577e891c9443c0367e9f9fa167c16
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-05-19 23:19:08 -0500

    Update 0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,13 +3,13 @@
 
 def minOperations(n):
     p = 2
-    integer = 0
+    num = 0
     if type(n) != int or n <= 1:
         return 0
     while n != 1:
         if n % p == 0:
             n = n / p
-            integer = integer + p
-            else:
-                p = p + 1
-      return integer
+            num = num + p
+        else:
+            p = p + 1
+    return int(num)
