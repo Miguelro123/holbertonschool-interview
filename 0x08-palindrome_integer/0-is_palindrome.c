e7c18bd5098a637bffe55e6642164145e8ad4e0f
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-10-28 21:34:06 -0500

    is_palindrome

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,24 +1,33 @@
-/**
- * is_palindrome - function that checks
- * whether or not a given unsigned integer
- * is a palindrome.
- * @n: unsigned long number to check
- * Return: 1 if is palindrome or 0 if not
- */
-
-int is_palindrome(unsigned long n)
+# incluye  " palindrome.h "
+# incluye  < stdio.h >
+/ * *
+ * is_palindrome - función si es palíndromo
+ * @n: entero para comprobar si es palíndromo
+ * Devuelve: 1 si n es un palíndromo y 0 en caso contrario
+ * /
+int  is_palindrome ( unsigned  long n)
 {
-    unsigned long x = 0;
-    unsigned long aux = n;
+	int unidad = 0 ;
+	/ * unsigned long int dec = 1; * /
+	/ * unsigned long int dec_up = 1; * /
+	unsigned  long  int revn = 0 ;
+	unsigned  long  int original = n;
 
-    while (aux != 0)
-    {
-        x += aux % 10;
-        aux /= 10;
-        if (aux != 0)
-            x *= 10;
-    }
-    if (x == n)
-        return (1);
-    return (0);
+	/ * mientras (n> dec) * /
+	/ * { * /
+	/ * dec = dec * 10; * /
+	/ * } * /
+	/ * si (dec> 1) * /
+	/ * dec = dec / 10; * /
+	mientras (n> = 1 )
+	{
+
+		unidad = n% 10 ;
+		/ * n = n - (unidad * dec); * /
+		revn = (revn * ( 10 )) + unidad;
+		/ * dec_up = dec_up * 10; * /
+		n = n / 10 ;
+	}
+	/ * printf ("este original% lu este vuelta% lu \ n", original, revn); * /
+	return (original == revn);
 }
