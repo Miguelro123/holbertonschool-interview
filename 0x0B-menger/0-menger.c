d52ce8f5d75c845d56709635e9e5dfdd19544105
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-11-05 10:29:10 -0500

    2D Menger sponge

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,159 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "slide_line.h"
-
+#include "menger.h"
 /**
- * zeros_right - Remove zeros
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
+ * menger - function that draws a 2D menger sponge
+ * @level: level of the menger sponge to draw
  */
-void zeros_right(int *line, size_t size)
+void menger(int level)
 {
-	size_t i;
-	int found, zeros, aux, tmp, act;
+	int n, m, j, x, y;
+	char s;
 
-	i = size - 1;
-	do {
-		found = 0;
-		zeros = 0;
-		for (act = i; act >= 0; act--)
-		{
-/*printf(">%d\n}1;5202;0c", line[act]);*/
-			if (line[act] == 0)
-			{
-				zeros += 1;
-/*printf("ZEROOOO\n");*/
-			}
-			else if (line[act] != 0)
-			{
-/*printf("FOUND\n");*/
-				found = 1;
-				break;
-			}
-		}
-		aux = i - zeros;
-		if (aux >= 0)
-		{
-			tmp = line[i];
-			line[i] = line[aux];
-			line[aux] = tmp;
-		}
-		i--;
-	} while (found);
-}
-
-/**
- * add_right - move right
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- */
-
-void add_right(int *line, size_t size)
-{
-	size_t i;
-	int aux;
-
-	for (i = size - 1; i > 0; i--)
+	m = pow(3, level);
+	for (n = 0; n < m; n++)
 	{
-		aux = i - 1;
-		if (line[i] == line[aux])
+		for (j = 0; j < m;)
 		{
-			line[i] += line[aux];
-			line[aux] = 0;
-		}
-	}
-}
-
-/**
- * add_left - move left
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- */
-void add_left(int *line, size_t size)
-{
-	size_t i;
-	int aux;
-
-	for (i = 0; i < (size - 1); i++)
-	{
-		aux = i + 1;
-		if (line[i] == line[aux])
-		{
-			line[i] += line[aux];
-			line[aux] = 0;
-		}
-	}
-}
-
-/**
- * zeros_left - Remove zeros
- *
- * @line: Pointer to the array of int
- * @size: Number of elements in @line
- */
-void zeros_left(int *line, size_t size)
-{
-	int found, zeros, aux, tmp, i, act, s;
-
-	s = size;
-	i = 0;
-	do {
-		found = 0;
-		zeros = 0;
-		for (act = i; act < s; act++)
-		{
-/*printf(">%d\n}1;5202;0c", line[act]);*/
-			if (line[act] == 0)
+			s = '#';
+			x = n;
+			y = j++;
+			while (x > 0 || y > 0)
 			{
-				zeros += 1;
-/*printf("ZEROOOO\n");*/
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					s = ' ';
+				}
+				x /= 3;
+				y /= 3;
 			}
-			else if (line[act] != 0)
-			{
-	    /*printf("FOUND\n");*/
-				found = 1;
-				break;
-			}
+			printf("%c", s);
 		}
-		aux = i + zeros;
-		if (aux < s)
-		{
-			tmp = line[i];
-			line[i] = line[aux];
-			line[aux] = tmp;
-		}
-		i++;
-	} while (found);
-}
-
-/**
- * slide_line - move the numbers
- *
- * @line: Line to count
- * @size: Length of numbers
- * @direction: Right or left
- * Return: 1 on success 0 on failure
- */
-
-int slide_line(int *line, size_t size, int direction)
-{
-	if (direction == 0)
-	{
-		zeros_left(line, size);
-		add_left(line, size);
-		zeros_left(line, size);
+		printf("\n");
 	}
-	else if (direction == 1)
-	{
-		zeros_right(line, size);
-		add_right(line, size);
-		zeros_right(line, size);
-	}
-	else
-		return (0);
-	return (1);
 }
