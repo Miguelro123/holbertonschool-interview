8730743fd34ec79285654367fee1ebdd515a64cf
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-11-05 10:29:48 -0500

    2D Menger sponge

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,14 +1,6 @@
-#ifndef _SLIDE_LINE_H
-#define _SLIDE_LINE_H
-
-#include <stdlib.h>
+#ifndef MENGER_SPONGE_H
+#define MENGER_SPONGE_H
+#include <math.h>
 #include <stdio.h>
-
-#define SLIDE_LEFT 0
-#define SLIDE_RIGHT 1
-
-void left_order(int *line, size_t size);
-void reverse_array(int *a, int n);
-int slide_line(int *line, size_t size, int direction);
-
+void menger(int level);
 #endif
