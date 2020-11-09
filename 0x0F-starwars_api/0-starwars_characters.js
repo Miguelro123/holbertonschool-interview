23fb13515b9826749b3fccd626936a442e344ed0
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-11-09 11:17:54 -0500

    Star Wars Characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const actor in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[actor], (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
+  }
+});
