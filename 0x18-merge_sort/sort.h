a787e58e3c58775d4b3ee1ff9a2918f43b11e049
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-11-09 20:27:17 -0500

    Update

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef H_MERGE_SORT
+#define H_MERGE_SORT
+
+/** Libraries */
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/** Prototypes */
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+#endif
